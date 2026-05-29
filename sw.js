50fdaed85f8093948991ec0c605eca7428741b93
KJ-Paul <65453500+KJ-Paul@users.noreply.github.com>
2026-05-29 11:39:21 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'courtready-v1';
+const ASSETS = [
+  '/courtready/',
+  '/courtready/index.html',
+  '/courtready/manifest.json',
+  '/courtready/icon-192.png',
+  '/courtready/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;900&family=Barlow:wght@400;500;600&display=swap',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  // Always network-first for Whoop API
+  if (e.request.url.includes('whoop.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const net = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || net;
+    })
+  );
+});
+
+// Notification click handler
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/courtready/'));
+});
